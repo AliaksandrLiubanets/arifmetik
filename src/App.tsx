042e7df380b47e9e05087619c1b0f9364453ba3b
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-08-17 08:47:48 +0300

    Change App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import './App.css';
-import {Settings} from './components/Settings'
+import React from 'react'
+import './App.css'
+import {Settings} from './components/Settings/Settings'
+import {Game} from './components/Game/Game'
 
 function App() {
-  return (
-    <div className="App">
-      <Settings />
-    </div>
-  );
+    const [isStarted, setIsStarted] = React.useState(false)
+    const [numberComp, setNumberComp] = React.useState(10)
+    const [timeoutValue, setTimeoutValue] = React.useState(0.8)
+    const [actionsCount, setActionsCount] = React.useState(2)
+    const [restart, setRestart] = React.useState(false)
+
+    return (
+        <div className="App">
+            {isStarted
+                ? <Game numberComp={numberComp}
+                        timeoutValue={timeoutValue}
+                        actionsCount={actionsCount}
+                        setIsStarted={setIsStarted}
+                        setRestart={setRestart}
+                        restart={restart}
+                />
+                : <Settings setIsStarted={setIsStarted}
+                            restart={restart}
+                            actionsCount={actionsCount}
+                            timeoutValue={timeoutValue}
+                            numberComp={numberComp}
+                            setActionsCount={setActionsCount}
+                            setNumberComp={setNumberComp}
+                            setTimeoutValue={setTimeoutValue}
+                            setRestart={setRestart}
+                />
+            }
+        </div>
+    )
 }
 
-export default App;
+export default App
