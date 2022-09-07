1ba172b50581d6b4fe2e1d546276a6c157051974
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-09-07 09:27:23 +0300

    Add flash-cards in assets

--- src/components/FlashCards/FlashCards.tsx
+++ src/components/FlashCards/FlashCards.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import s from './FlashCards.module.css'
-import r_1 from '../../assets/flash-cards/1_r.png'
-import r_2 from '../../assets/flash-cards/2_r.png'
-import r_3 from '../../assets/flash-cards/3_r.png'
+import r_2 from '../../assets/flash-cards/9_r_4_3_2.png'
+import r_3 from '../../assets/flash-cards/9_r_4_2_3.png'
 
 export const FlashCards = () => {
     return <div className={s.flash}>
