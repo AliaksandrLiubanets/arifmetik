d3912f40c1c10e9f711777ac38a3c659f5c2676a
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-09-14 21:48:25 +0300

    Add icon for flashcards in main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import {PATH} from '../../enums/paths'
+import dominoes from '../../assets/main-icons/dominoes.png'
 
 export const Main = () => {
 
     return <>
         {/*<div><NavLink to={PATH.MAIN}>На главную</NavLink></div>*/}
-        <div><NavLink to={PATH.FLASH}>Флэшкарты</NavLink></div>
+        <div>
+            <NavLink to={PATH.FLASH}>
+                <div>
+                    <div>Флэшкарты</div>
+                    <img src={dominoes} alt={'dominoes_icon'}/>
+                </div>
+            </NavLink>
+        </div>
         <div><NavLink to={PATH.COUNT}>Счёт</NavLink></div>
     </>
 }
