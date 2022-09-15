d017b09b5318943e09028de609ec6794d72b56d5
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-09-15 08:42:54 +0300

    Add styles for main_icons

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import {PATH} from '../../enums/paths'
-import dominoes from '../../assets/main-icons/dominoes.png'
+import dominoes from '../../assets/main-icons/dominoes_2.jpg'
+import digits from '../../assets/main-icons/digits_1.jpg'
+import s from './Main.module.css'
 
 export const Main = () => {
 
-    return <>
-        {/*<div><NavLink to={PATH.MAIN}>На главную</NavLink></div>*/}
-        <div>
+    return <div className={s.content}>
+        <div className={s.icon_link}>
             <NavLink to={PATH.FLASH}>
-                <div>
-                    <div>Флэшкарты</div>
+                <div className={s.item}>
                     <img src={dominoes} alt={'dominoes_icon'}/>
+                    <div>Флэшкарты</div>
                 </div>
             </NavLink>
         </div>
-        <div><NavLink to={PATH.COUNT}>Счёт</NavLink></div>
-    </>
+        <div className={s.icon_link}>
+            <NavLink to={PATH.COUNT}>
+                <div className={s.item}>
+                    <img src={digits} alt={'digits_icon'}/>
+                    <div>Счёт</div>
+                </div>
+            </NavLink>
+        </div>
+    </div>
 }
