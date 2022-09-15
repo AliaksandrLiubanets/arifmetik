f2ab7a330d365cce35fbc9d0c1918942903b7169
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-09-15 08:52:13 +0300

    Add link to main_page from Error404

--- src/components/Error404/Error404.tsx
+++ src/components/Error404/Error404.tsx
@@ -1,6 +1,11 @@
+import {NavLink} from 'react-router-dom'
+import {PATH} from '../../enums/paths'
+import React from 'react'
+
 export const Error404 = () => {
 
-    return (
-        <div>Page not found!</div>
-    )
+    return <>
+            <div>Page not found!</div>
+            <div>Go to <NavLink to={PATH.MAIN}>main page</NavLink></div>
+        </>
 }
