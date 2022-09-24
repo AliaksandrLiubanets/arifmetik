54c91d214a15d2a5448f94d18a7d743612bf56f4
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-09-24 21:09:58 +0300

    Add react-sound and @types

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -6,6 +6,7 @@ import {AppRootStateType} from '../../store/store'
 import ReactAudioPlayer from 'react-audio-player'
 import plus_2 from '../../assets/sounds/actions/+2.mp3'
 import {ActionAndSoundType} from '../../store/countGameReducer'
+import Sound from 'react-sound'
 
 type Props = {
     showInput: (isShowAnswer: boolean) => void
@@ -25,138 +26,136 @@ export const Actions: FC<Props> = memo(({focusOnElement, showInput}) => {
         } = useSelector((state: AppRootStateType) => state.count)
 
         let actionSound: () => void
-        switch (calc) {
-            case '+1':
-                actionSound = () => p_1()
-                break
-            case '+2':
-                actionSound = () => p_2()
-                break
-            case '+3':
-                actionSound = () => p_3()
-                break
-            case '+4':
-                actionSound = () => p_4()
-                break
-            case '+5':
-                actionSound = () => p_5()
-                break
-            case '+6':
-                actionSound = () => p_6()
-                break
-            case '+7':
-                actionSound = () => p_7()
-                break
-            case '+8':
-                actionSound = () => p_8()
-                break
-            case '+9':
-                actionSound = () => p_9()
-                break
-            case '+10':
-                actionSound = () => p_10()
-                break
-            case '+11':
-                actionSound = () => p_11()
-                break
-            case '+12':
-                actionSound = () => p_12()
-                break
-            case '+13':
-                actionSound = () => p_13()
-                break
-            case '+14':
-                actionSound = () => p_14()
-                break
-            case '+15':
-                actionSound = () => p_15()
-                break
-            case '+16':
-                actionSound = () => p_16()
-                break
-            case '+17':
-                actionSound = () => p_17()
-                break
-            case '+18':
-                actionSound = () => p_18()
-                break
-            case '+19':
-                actionSound = () => p_19()
-                break
-            case '+20':
-                actionSound = () => p_20()
-                break
-            case '-1':
-                actionSound = () => m_1()
-                break
-            case '-2':
-                actionSound = () => m_2()
-                break
-            case '-3':
-                actionSound = () => m_3()
-                break
-            case '-4':
-                actionSound = () => m_4()
-                break
-            case '-5':
-                actionSound = () => m_5()
-                break
-            case '-6':
-                actionSound = () => m_6()
-                break
-            case '-7':
-                actionSound = () => m_7()
-                break
-            case '-8':
-                actionSound = () => m_8()
-                break
-            case '-9':
-                actionSound = () => m_9()
-                break
-            case '-10':
-                actionSound = () => m_10()
-                break
-            case '-11':
-                actionSound = () => m_11()
-                break
-            case '-12':
-                actionSound = () => m_12()
-                break
-            case '-13':
-                actionSound = () => m_13()
-                break
-            case '-14':
-                actionSound = () => m_14()
-                break
-            case '-15':
-                actionSound = () => m_15()
-                break
-            case '-16':
-                actionSound = () => m_16()
-                break
-            case '-17':
-                actionSound = () => m_17()
-                break
-            case '-18':
-                actionSound = () => m_18()
-                break
-            case '-19':
-                actionSound = () => m_19()
-                break
-            case '-20':
-                actionSound = () => m_20()
-                break
-            default:
-                actionSound = () => w()
-        }
+        // switch (calc) {
+        //     case '+1':
+        //         actionSound = () => p_1()
+        //         break
+        //     case '+2':
+        //         actionSound = () => p_2()
+        //         break
+        //     case '+3':
+        //         actionSound = () => p_3()
+        //         break
+        //     case '+4':
+        //         actionSound = () => p_4()
+        //         break
+        //     case '+5':
+        //         actionSound = () => p_5()
+        //         break
+        //     case '+6':
+        //         actionSound = () => p_6()
+        //         break
+        //     case '+7':
+        //         actionSound = () => p_7()
+        //         break
+        //     case '+8':
+        //         actionSound = () => p_8()
+        //         break
+        //     case '+9':
+        //         actionSound = () => p_9()
+        //         break
+        //     case '+10':
+        //         actionSound = () => p_10()
+        //         break
+        //     case '+11':
+        //         actionSound = () => p_11()
+        //         break
+        //     case '+12':
+        //         actionSound = () => p_12()
+        //         break
+        //     case '+13':
+        //         actionSound = () => p_13()
+        //         break
+        //     case '+14':
+        //         actionSound = () => p_14()
+        //         break
+        //     case '+15':
+        //         actionSound = () => p_15()
+        //         break
+        //     case '+16':
+        //         actionSound = () => p_16()
+        //         break
+        //     case '+17':
+        //         actionSound = () => p_17()
+        //         break
+        //     case '+18':
+        //         actionSound = () => p_18()
+        //         break
+        //     case '+19':
+        //         actionSound = () => p_19()
+        //         break
+        //     case '+20':
+        //         actionSound = () => p_20()
+        //         break
+        //     case '-1':
+        //         actionSound = () => m_1()
+        //         break
+        //     case '-2':
+        //         actionSound = () => m_2()
+        //         break
+        //     case '-3':
+        //         actionSound = () => m_3()
+        //         break
+        //     case '-4':
+        //         actionSound = () => m_4()
+        //         break
+        //     case '-5':
+        //         actionSound = () => m_5()
+        //         break
+        //     case '-6':
+        //         actionSound = () => m_6()
+        //         break
+        //     case '-7':
+        //         actionSound = () => m_7()
+        //         break
+        //     case '-8':
+        //         actionSound = () => m_8()
+        //         break
+        //     case '-9':
+        //         actionSound = () => m_9()
+        //         break
+        //     case '-10':
+        //         actionSound = () => m_10()
+        //         break
+        //     case '-11':
+        //         actionSound = () => m_11()
+        //         break
+        //     case '-12':
+        //         actionSound = () => m_12()
+        //         break
+        //     case '-13':
+        //         actionSound = () => m_13()
+        //         break
+        //     case '-14':
+        //         actionSound = () => m_14()
+        //         break
+        //     case '-15':
+        //         actionSound = () => m_15()
+        //         break
+        //     case '-16':
+        //         actionSound = () => m_16()
+        //         break
+        //     case '-17':
+        //         actionSound = () => m_17()
+        //         break
+        //     case '-18':
+        //         actionSound = () => m_18()
+        //         break
+        //     case '-19':
+        //         actionSound = () => m_19()
+        //         break
+        //     case '-20':
+        //         actionSound = () => m_20()
+        //         break
+        //     default:
+        //         actionSound = () => w()
+        // }
 
 
         // const [sound] = useSound(soundStr)
-        const [sound] = useSound(calc.sound)
+        // const [sound] = useSound(calc.sound)
         // const [ss] = useSound(plus_2)
-        console.log('plus_2:', plus_2)
-        console.log('calc.sound:', calc.sound)
-        actionSound = useCallback(() => sound(), [calc])
+        // actionSound = useCallback(() => sound(), [calc])
 
         useEffect(() => {
             if (index < actionsCount) {
@@ -194,13 +193,18 @@ export const Actions: FC<Props> = memo(({focusOnElement, showInput}) => {
 
 
         return <div className={s.container}>
+            <Sound
+                url={calc.sound}
+                playStatus={'PLAYING'}
+
+            />
             <div className={s.player}>
-                <ReactAudioPlayer
-                    controls={true}
-                    src={calc.sound}
-                    preload={'auto'}
-                    // onPlay={actionSound}
-                />
+                {/*<ReactAudioPlayer*/}
+                {/*    controls={true}*/}
+                {/*    src={calc.sound}*/}
+                {/*    preload={'auto'}*/}
+                {/*    onPlay={actionSound}*/}
+                {/*/>*/}
             </div>
             {/*<button onClick={actionSound}>Sound</button>*/}
             <div className={s.action}>{calc.action}</div>
